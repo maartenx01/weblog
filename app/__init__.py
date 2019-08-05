8882592959c6d5942ffcd74e00ea90203917c449
Menghe Qian <mmq2dr@virginia.edu>
2019-08-04 21:53:49 -0500

    password reset

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
@@ -24,13 +26,14 @@ if not app.debug:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
-            secure()
+            secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Weblog Failure',
-            credentials=auth, secure=secure
-        )
+            toaddrs=app.config['ADMINS'], 
+            subject='Weblog Failure',
+            credentials=auth, 
+            secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
     
--- config.py
+++ config.py
@@ -11,5 +11,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    MAIL_ADMIN = os.environ.get('MAIL_ADMIN')
+    ADMINS = [MAIL_ADMIN]
     POSTS_PER_PAGE = 25
