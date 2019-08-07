8b5288f02ed791d256dbc35b84c2ac661ac2ab1d
Menghe Qian <mmq2dr@virginia.edu>
2019-08-07 10:56:51 -0500

    heroku config

--- config.py
+++ config.py
@@ -6,12 +6,19 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_ADMIN = os.environ.get('MAIL_ADMIN')
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_ADMIN = os.environ.get('MAIL_ADMIN')
+    # ADMINS = [MAIL_ADMIN]
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = "587"
+    MAIL_USE_TLS = "1"
+    MAIL_USERNAME = "snailmaildaemon"
+    MAIL_PASSWORD = "Shoreland#19#"
+    MAIL_ADMIN = "snailmaildaemon@gmail.com"
     ADMINS = [MAIL_ADMIN]
     POSTS_PER_PAGE = 25
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
